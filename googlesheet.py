1eb8873c93363934590bddbca59a00ae858c71dc
minsuki2 <giromi@cau.ac.kr>
2022-03-24 17:45:52 +0900

    finish recieve

--- googlesheet.py
+++ googlesheet.py
@@ -1,5 +1,31 @@
 from oauth2client.service_account import ServiceAccountCredentials
 import gspread
+import src_info as si
+
+def import_gsheet(spreadsheet_url, timeidx):
+    credential = ServiceAccountCredentials.from_json_keyfile_name(json_key_path, scope)
+    gc = gspread.authorize(credential)
+    doc = gc.open_by_url(spreadsheet_url)
+    worksheet = doc.worksheet("벌금명단")
+    col_data = worksheet.col_values(timeidx)
+    return (col_data[2:])
+
+def get_later(spreadsheet_url, timeidx):
+    vote_later = []
+    question_later = []
+    attend_later = []
+    bits_data = import_gsheet(spreadsheet_url, timeidx)
+    i = 0
+    for result in bits_data:
+        if result[0] == '1':
+            vote_later.append(si.UserID.users_name[i])
+        if result[1] == '1':
+            question_later.append(si.UserID.users_name[i])
+        if result[2] == '1':
+            attend_later.append(si.UserID.users_name[i])
+        i += 1
+    return (vote_later, question_later, attend_later)
+
 
 scope = [
     "https://spreadsheets.google.com/feeds",
@@ -7,13 +33,24 @@ scope = [
 ]
 
 json_key_path = "./logical-tea-345007-5edc511aecb4.json"	# JSON Key File Path
+spreadsheet_url = "https://docs.google.com/spreadsheets/d/1DuBSkKT665lYLiatWSIZ94NssrhH5ErJQheUdFuPYKk/edit#gid=1319193929"
 
-credential = ServiceAccountCredentials.from_json_keyfile_name(json_key_path, scope)
-gc = gspread.authorize(credential)
+timeidx = 2
+v, q, a = get_later(spreadsheet_url, timeidx)
+print("투표 지각 :")
+print(v)
+print()
+print("질문 지각 :")
+print(q)
+print()
+print("참석 지각 :")
+print(a)
+print()
+#------------------------
+# 쓰기
+#  worksheet.update_acell('B3', '= CONCATENATE("00", "1")')
 
 # URL로 열기
-spreadsheet_url = "https://docs.google.com/spreadsheets/d/1DuBSkKT665lYLiatWSIZ94NssrhH5ErJQheUdFuPYKk/edit#gid=1319193929"
-doc = gc.open_by_url(spreadsheet_url)
 
 #  #OR
 #
@@ -27,7 +64,6 @@ doc = gc.open_by_url(spreadsheet_url)
 #  doc = gc.open("test")
 
 # Sheet 선택
-worksheet = doc.worksheet("벌금명단")
 #
 #  # 모든 값을 list로 가져오기
 #  list_of_lists = worksheet.get_all_values()
@@ -42,42 +78,14 @@ worksheet = doc.worksheet("벌금명단")
 #  print(value_a1)
 #
 #  # 행 값 가져오기
-row_data = worksheet.row_values(2)
-print(row_data[1:])
+#  row_data = worksheet.row_values(2)
+#  print(row_data[1:])
 
 # 열 값 가져오기
-col_data = worksheet.col_values(2)
-print(col_data[2:])
-col_data = worksheet.col_values(3)
-print(col_data[2:])
+#  print(col_data[2:])
+#  col_data = worksheet.col_values(3)
+#  print(col_data[2:])
 
 # 열 값 가져오기
 #  rang_data = worksheet.get_values("B3:B7")
 #  print(rang_data)
-vote_later = []
-question_later = []
-attend_later = []
-
-i = 0
-data = ['101', '010', '100'];
-for result in data :
-    print(result)
-    if result[0] == '1':
-        print(result[0])
-        vote_later.append(i)
-    if result[1] == '1':
-        print(result[1])
-        question_later.append(i)
-    if result[2] == '1':
-        print(result[2])
-        attend_later.append(i)
-    i += 1
-
-print("투표 지각 :", vote_later)
-print("질문 지각 :", question_later)
-print("참석 지각 :", attend_later)
-
-
-#------------------------
-# 쓰기
-#  worksheet.update_acell('B3', '= CONCATENATE("00", "1")')
---
+++ src_info.py
@@ -0,0 +1,106 @@
+class WebHookUrl:
+	announcement_webhook = "https://hooks.slack.com/services/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"
+	question_webhook = "https://hooks.slack.com/services/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"
+	penalty_webhook = "https://hooks.slack.com/services/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"
+	cash_fit_webhook = "https://hooks.slack.com/services/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"
+
+class BotOAuth:
+	user_token = "xoxp-1830334374899-1853957534688-3251494072295-b70e8bf89cc4d9a0c416d14d3075940e"
+	bot_token = "xoxb-1830334374899-2443966688067-w0MZL8043NzMuxjWzAhtK4mw"
+
+class ChannelID:
+	announcement = "C01PZAK0NGP"
+	book_rec = "C02M3SU81K2"
+	question = "C027UQYFH8U"
+	penalty = "C029TP5DS0G"
+	cash_fit = "C02QY3M5F7H"
+
+class UserID:
+	users_name = [
+				"강동운",
+				"김가현",
+				"김민수",
+				"김범태",
+				"김성훈",
+				"김주영",
+				"문건호",
+				"박상아",
+				"박상진",
+				"박서지",
+				"박재형",
+				"박혜빈",
+				"서수희",
+				"신환종",
+				"심혜빈",
+				"양희재",
+				"윤영주",
+				"이재원",
+				"이현정",
+				"장호진",
+				"전진표",
+				"정하진",
+				"정현진",
+				"천지영",
+				"최명원",
+				"한수경",
+				"하승희"
+			]
+	user_id_to_name = {
+				"U02RL2XV4KH" : "강동운",
+				"U02RTQ2NXKQ" : "김가현",
+				"U02LE7HKCE6" : "김민수",
+				"U027JQK00KB" : "김범태",
+				"U01QT3KFU4D" : "김성훈",
+				"U026RFKEN3H" : "김주영",
+				"U02733TKY9K" : "문건호",
+				"U02S33JRS9J" : "박상아",
+				"U02S4TERJ5S" : "박상진",
+				"U02S2LM4LP4" : "박서지",
+				"U02S0M72N82" : "박재형",
+				"U01QMTD1K37" : "박혜빈",
+				"U02S2PVS3B6" : "서수희",
+				"U01QSTF6M18" : "신환종",
+				"U02RU9NTCA2" : "심혜빈",
+				"U01QEPH2TP1" : "양희재",
+				"U01QLRF7MJ6" : "윤영주",
+				"U02AKLMDSRE" : "이재원",
+				"U02RTS9357G" : "이현정",
+				"U01R3U5FQL8" : "장호진",
+				"U02S3UAPS9J" : "전진표",
+				"U02SR22Q9MW" : "정하진",
+				"U02727PASNS" : "정현진",
+				"U032CUPKA95" : "천지영",
+				"U02S0ER3W9H" : "최명원",
+				"U027382N5K7" : "한수경",
+				"U02RU5T3KSS" : "하승희"
+			}
+	user_name_to_id = {
+				"강동운" : "U02RL2XV4KH",
+				"김가현" : "U02RTQ2NXKQ",
+				"김민수" : "U02LE7HKCE6",
+				"김범태" : "U027JQK00KB",
+				"김성훈" : "U01QT3KFU4D",
+				"김주영" : "U026RFKEN3H",
+				"문건호" : "U02733TKY9K",
+				"박상아" : "U02S33JRS9J",
+				"박상진" : "U02S4TERJ5S",
+				"박서지" : "U02S2LM4LP4",
+				"박재형" : "U02S0M72N82",
+				"박혜빈" : "U01QMTD1K37",
+				"서수희" : "U02S2PVS3B6",
+				"신환종" : "U01QSTF6M18",
+				"심혜빈" : "U02RU9NTCA2",
+				"양희재" : "U01QEPH2TP1",
+				"윤영주" : "U01QLRF7MJ6",
+				"이재원" : "U02AKLMDSRE",
+				"이현정" : "U02RTS9357G",
+				"장호진" : "U01R3U5FQL8",
+				"전진표" : "U02S3UAPS9J",
+				"정하진" : "U02SR22Q9MW",
+				"정현진" : "U02727PASNS",
+				"천지영" : "U032CUPKA95",
+				"최명원" : "U02S0ER3W9H",
+				"한수경" : "U027382N5K7",
+				"하승희" : "U02RU5T3KSS"
+	}
+	
